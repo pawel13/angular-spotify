416f4763af647599cea0025dd187f17a1c025c81
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-26 12:21:00 +0100

    Basic styles, bootstrap

--- src/app/playlists/components/list-item/list-item.component.ts
+++ src/app/playlists/components/list-item/list-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+  styleUrls: ['./list-item.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ListItemComponent implements OnInit {
 
