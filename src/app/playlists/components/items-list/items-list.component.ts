a026f290224ec2a4c128c058b8b9bf419d6e27b4
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-26 15:59:40 +0100

    Long version of ngFOr

--- src/app/playlists/components/items-list/items-list.component.ts
+++ src/app/playlists/components/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist } from 'src/app/model/Playlist';
 
 @Component({
   selector: 'app-items-list',
@@ -9,6 +10,26 @@ export class ItemsListComponent implements OnInit {
 
   constructor() { }
 
+  playlists: Playlist[] = [
+    {
+    id:1,
+    name:'Hits 2018',
+    favourite:true,
+    color:'#0000ff'
+  },
+  {
+    id:2,
+    name:'Hits 2017',
+    favourite:false,
+    color:'#0faa0ff'
+  },
+  {
+    id:3,
+    name:'Hits 2016',
+    favourite:false,
+    color:'#df00ff'
+  }
+];
   ngOnInit() {
   }
 
