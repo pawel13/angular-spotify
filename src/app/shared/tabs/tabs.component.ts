18a7f71300700a798f599ffa44b614307afdb7b3
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-27 16:01:14 +0100

    Access in parent to child by reference (#)

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
+import { TabsNavComponent } from '../tabs-nav/tabs-nav.component';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterViewInit {
   tabsList:TabComponent[] = []
+
+  @ViewChild('navRef')
+  navRef: TabsNavComponent;
 
   toggle(active:TabComponent){
     this.tabsList.forEach( tab => {
@@ -17,6 +21,12 @@ export class TabsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('tabs onInit', this.navRef.tabs);
+
+  }
+  
+  ngAfterViewInit(){
+    console.log('tabsAfterViewInit', this.navRef.tabs);
   }
 
 }
