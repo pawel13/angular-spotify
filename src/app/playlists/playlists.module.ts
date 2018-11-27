7d695306c3bf8ab9cd2192cdec591a9b140c0314
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-27 11:27:11 +0100

    Custom directive in shared module

--- src/app/playlists/playlists.module.ts
+++ src/app/playlists/playlists.module.ts
@@ -5,12 +5,14 @@ import { ItemsListComponent } from './components/items-list/items-list.component
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { PlaylistDetailsComponent } from './components/playlist-details/playlist-details.component';
 import {FormsModule} from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PlaylistsViewComponent, ItemsListComponent, ListItemComponent, PlaylistDetailsComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   exports: [PlaylistsViewComponent],
 })
