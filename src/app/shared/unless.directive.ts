827fa7e3b11062dd87b17057538634ff2a74ffea
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-27 14:57:53 +0100

    Generate tabs

---
+++ src/app/shared/unless.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, TemplateRef, ViewContainerRef, ComponentFactoryResolver, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+
+  @Input()
+  set appUnless(hide:boolean){
+    if(!hide){
+      this.vcr.createEmbeddedView(
+        this.tpl,
+        {
+          message: 'placki',
+          $implicit: 'Ala ma kota'
+        }, this.vcr.length
+      );
+    }
+  }
+  constructor(
+    private tpl: TemplateRef<any>,
+    private vcr: ViewContainerRef,
+    ) { 
+  }
+
+
+
+}
