481cbd0e47bdb7d23d5bf1a7efdd2fe606fca762
pawel13 <szczepanowski.pawel13@gmail.com>
2018-11-29 10:47:52 +0100

    Aynchronous validator with custom Observable, complete, unsubsribe, pending message

--- src/app/music/components/search-form/search-form.component.ts
+++ src/app/music/components/search-form/search-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl, AsyncValidatorFn } from '@angular/forms';
 import { distinctUntilChanged, filter, throttle, debounceTime } from 'rxjs/operators';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-search-form',
@@ -16,17 +17,37 @@ export class SearchFormComponent implements OnInit {
       (control:AbstractControl): ValidationErrors | null => {
         const hasError = (control.value as string).includes(badword);
         return hasError ? {
-        censor: { badword }
+        censor: {badword} // same as {badword: badword}
       } : null; // adds key to form error field
     };
+
+    const asyncCensor = (badword: string): AsyncValidatorFn =>
+      (control:AbstractControl): Observable<ValidationErrors | null> => {
+      
+        return Observable.create((observer: Observer<ValidationErrors|null>) => {
+          setTimeout( () => {
+            const hasError = (control.value as string).includes(badword);
+            observer.next(
+              hasError ? {
+                censor: {badword}
+              } : null
+            );  
+            observer.complete();
+          }, 2000);
+        });
+      };
 
 
     this.queryForm = new FormGroup({
       query: new FormControl('', [ 
         Validators.required,
         Validators.minLength(3),
-        censor('batman')
-      ])
+        //censor('batman')
+      ],[
+        asyncCensor('batman')
+      ]
+      
+      )
     });
     this.queryForm.get('query')!.valueChanges
     .pipe(
